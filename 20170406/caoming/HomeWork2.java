bbd74e70eeb8d2234d5c145dd76cc1936cab37e1
CaoMing <501884928@qq.com>
2017-04-06 21:27:15 +0800

    Update HomeWork2.java

--- 20170406/caoming/HomeWork2.java
+++ 20170406/caoming/HomeWork2.java
@@ -7,7 +7,7 @@ public class HomeWork2
 		System.out.print("华氏度为 : ");
 		double tF = input.nextInt();     
 		double t=(tF-32)/1.8;
-    System.out.println("摄氏度为Ç : "+t);
+    System.out.println("摄氏度为 : "+t);
 		
 	}
 }	
