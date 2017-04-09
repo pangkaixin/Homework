79263a321645a7b0969837a468a33129f02642d2
CaoMing <501884928@qq.com>
2017-04-09 14:00:51 +0800

    Add files via upload

---
+++ 20170407/caoming/Exercise0408.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Exercise0408
+{
+public static void main(String[] args)
+	{
+	    
+			
+			Scanner input=new Scanner(System.in);
+			System.out.println("是否是会员（温馨提示请输入 是/否 乱输入价钱加倍(⊙o⊙)哦）：");
+			String vip=input.nextLine();
+			System.out.print("消费金额：");
+			double money=input.nextInt();
+			
+			
+	
+	
+	
+	
+	
+	}
+}
+	
+	
+	
