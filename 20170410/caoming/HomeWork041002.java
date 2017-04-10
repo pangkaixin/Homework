b6581b42b32851613727a6114c6fa1c869aa1400
CaoMing <501884928@qq.com>
2017-04-10 22:43:43 +0800

    Add files via upload

---
+++ 20170410/caoming/HomeWork041002.java
@@ -0,0 +1,45 @@
+﻿import java.util.Scanner;
+
+public class HomeWork041002    //根据输入数字显示对应的英文星期名称缩写。
+{
+		public static void main(String[] args)
+		{			
+        boolean hasput=true;
+          
+        while(hasput)
+        {
+        		Scanner sc=new Scanner(System.in);
+				System.out.print("please input the number(1-7):");
+			    int nub=sc.nextInt(); 
+				
+          	    switch(nub)
+				{
+						case 1:
+								System.out.println("today is MON");
+								break;
+						case 2:
+								System.out.println("today is TUE");
+		                        break;
+						case 3:
+								System.out.println("today is WED");	
+								break;
+                        case 4:
+								System.out.println("today is THU");
+								break;
+						case 5:
+								System.out.println("today is FRI");
+		                        break;
+						case 6:
+								System.out.println("today is SAT");	
+								break;
+						case 7:
+								System.out.println("today is SUN");	
+								break;
+						default:
+								System.out.println("Please input the right number(1-7)!");
+				}
+          
+        }          
+	
+		}
+}
