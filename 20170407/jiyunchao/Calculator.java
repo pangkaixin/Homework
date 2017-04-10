44d2881df8f2574dece30cab3b95a524d9c1bcbc
www65 <www65@Hasee>
2017-04-10 10:39:36 +0800

    作业修改

--- 20170407/jiyunchao/Calculator.java
+++ 20170407/jiyunchao/Calculator.java
@@ -1,6 +1,5 @@
 package com.jyc;
-//任务目标:实现一个计算器功能，从控制台分别输入两个数，还有还进行计算的
-//运算符（+-*/)然后输出相应结果。
+
 import java.util.Scanner;
 
 public class Calculator
@@ -12,13 +11,22 @@ public class Calculator
 		double number1 = input.nextDouble();
 		System.out.println("请输入第二个数:");
 		double number2 = input.nextDouble();
+		System.out.println("请输入运算符:");
+		String  operator=input.next();
 		double plus = number1 + number2;
 		double reduce = number1 - number2;
 		double ride = number1 * number2;
 		double except = number1 / number2;
-		System.out.println("这两个数的和是:" + plus);
-		System.out.println("这两个数的差是:" + reduce);
-		System.out.println("这两个数的积是:" + ride);
-		System.out.println("这两个数的商是:" + except);
+		
+		switch(operator)
+		{
+		case "+" : System.out.println("这两个数的和是:" + plus);break;
+		case "-" :System.out.println("这两个数的差是:" + reduce );break;
+		case "*" :System.out.println("这两个数的积是:" + ride);break;
+		case "/" :System.out.println("这两个数的商是:" + except);break;
+		}
+		
+		
+		
 	}
 }
