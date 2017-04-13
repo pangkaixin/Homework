7602c9c6e2bbca7516722ab8ee6769f3b6b3b8d2
Administrator <Administrator@PC-20161008SEAM>
2017-04-13 09:55:15 +0800

    修改作业

--- 20170412/changxu/MaxMin.java
+++ 20170412/changxu/MaxMin.java
@@ -2,25 +2,24 @@ public class MaxMin
 {
 	public static void main(String[] args)
 	{
-		int max, min;
-		int[] a =
-		{ 10, 8, 200, 98, 46, 12, 1 };
-		min = a[0];
-		max = a[0];
-		for (int i = 0; i < a.length; i++)
+		int max,min;
+		int[] a={10,8,200,98,46,12,1};
+		min=a[0];
+		max=a[0];
+		for(int i=0;i<a.length;i++)
 		{
-			if (a[i] > max)
+			if(a[i]>max)
 			{
-				max = a[i];
+				 max=a[i];				
 			}
-			if (a[i] < min)
+			if(a[i]<min)
 			{
-				min = a[i];
+				 min=a[i];
 			}
-
+			
 		}
-		System.out.println("max:" + max);
-		System.out.println("min:" + min);
-
-	}
+		System.out.println("max:"+max);	
+		System.out.println("min:"+min);
+		
+	} 
 }
