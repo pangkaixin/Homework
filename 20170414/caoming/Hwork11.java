b7d2d6812c840d4c0cd8dbe03fc17475aa322a83
CaoMing <501884928@qq.com>
2017-04-17 09:24:36 +0800

    Update Hwork11.java

--- 20170414/caoming/Hwork11.java
+++ 20170414/caoming/Hwork11.java
@@ -5,11 +5,11 @@ public class Hwork11
 	public static void main(String[] args)
 	{
 		String[] s1={"{","\n","\t","北京","\n","\t","编码:","101010100","\n","}"};
-		for(int i=0;i<10;i++)
+		for(int i=0;i<s1.length;i++)
 		{
 			StringBuffer a=new StringBuffer("");
 			a.append(s1[i]);
-			System.out.println(a.toString());
+			System.out.print(a.toString());
 		}
 		
 	}
