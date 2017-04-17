d84e2106c34cb3d69a28aaa29c9b5edd3bb15618
asus <asus@DESKTOP-5M7TD32>
2017-04-17 23:41:40 +0800

    提交作业

---
+++ 20170417/wanghanyang/Students.java
@@ -0,0 +1,14 @@
+package Work0417;
+
+public class Students
+{
+		String name;
+		int age;
+		String grade;
+		String hobby;
+		public void study()
+		{
+			System.out.println(name+"\r\n年龄："+age+"\r\n就读于："+grade+"\r\n就读于："+hobby);
+		}
+	
+}
