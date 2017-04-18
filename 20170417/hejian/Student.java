de57b9fc8ff9c14aa198abc88efe903d9d9dcaa9
HeJian <1486172312@qq.com>
2017-04-18 08:59:54 +0800

    Add files via upload
    修改作业

--- 20170417/hejian/Student.java
+++ 20170417/hejian/Student.java
@@ -2,15 +2,19 @@ public class Student
 {
 	String name;
 	String age;
+	String classs;
+	String hobby;
 	public void play()
 	{
-		System.out.println(name+"\n"+age);	
+		System.out.println(name+"\n"+age+"\n"+classs+"\n"+hobby);	
 	}
 	public static void main(String[] args)
 	{
 		Student student	=new Student();
 		student.name="张三";
 		student.age="18岁";
+		student.classs="三班";
+		student.hobby="看书";
 		student.play();
 	}
 }
--- 20170417/hejian/Teacher.java
+++ 20170417/hejian/Teacher.java
@@ -2,15 +2,19 @@ public class Teacher
 {
 	String name;
 	String age;
+	String major;
+	String course;
 	public void play()
 	{
-		System.out.println(name+"\n"+age);	
+		System.out.println(name+"\n"+age+"\n"+major+"\n"+course);	
 	}
 	public static void main(String[] args)
 	{
 		Teacher teacher	=new Teacher();
 		teacher.name="ÀîËÄ";
-		teacher.age="38Ëê";
+		teacher.age="10Äê";
+		teacher.major="java";
+		teacher.course="java";
 		teacher.play();
 	}
 }
